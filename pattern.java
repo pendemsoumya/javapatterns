f086f43ae5b50d53d2a67db743bae24de1bc1e02
pendemsoumya <166056253+pendemsoumya@users.noreply.github.com>
2025-04-14 20:21:37 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,24 @@
+public class pattern{
+    public static void main(String args[]){
+      /*  System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        */
+       //variable
+       String name = "soumya";
+       int age = 70;
+       double price = 25.67;
+       int a =25;
+       int b = 10;
+       b = 5;
+       name ="vaishu";
+
+
+
+
+    }
+     
+    }
+    
+
